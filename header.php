d96df2a589721034e7e8073673675261b270b80a
Patrick5029 <150467294+Patrick5029@users.noreply.github.com>
2023-11-16 21:24:39 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,13 @@
+ 
+<div class="logo_h">
+    <img src="/images/logo_kloarc.png" alt="logo" width="140" height="140">
+
+	<h1>KLO'ARC</h1>
+        <h2> Le tir à l'arc Cloharsien</h2>
+
+      <img src="/images/image_cible.jpg" alt="logo" width="128" height="128">
+</div>
+	
+
+
+
